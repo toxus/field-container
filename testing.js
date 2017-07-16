b0077a05d567951e58c6d060ac52d75ca846bcce
Jaap van der Kreeft <jaap@toxus.nl>
2017-07-16 16:37:54 +0200

    module exports

---
+++ testing.js
@@ -0,0 +1,35 @@
+"use strict";
+
+/**
+ * how it should work:
+ */
+
+const fieldClass = require('./lib/fieldContainer');
+
+let field1 = new fieldClass();
+let index = field1.add('nr1', "telephone", {value: "+31-0610810547"}, ["telephone.mobile"]);
+
+/*
+
+
+ function updateRecord() {
+ let share = findShare();
+
+ let record = new fieldClass();
+ let template = new templateClass();
+ template.readFile('./spec/rowTemplate.json');
+
+ // we now have bufer to import the information
+ record.importRow(jsonData, template);
+ record.stardardize();
+
+ // now we have the standard record that can be written to disk
+ // find the existing record
+ findShareData(share, record).then((shareData) => {
+
+ }).catch( (err) => {
+
+ });
+ }
+
+ */
