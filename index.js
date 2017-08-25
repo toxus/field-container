c7799d9d74a83bacf10b4d07665e778d5a7c6c67
Jaap van der Kreeft <jaap@toxus.nl>
2017-08-25 15:54:53 +0200

    export fieldtypes

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ module.exports = {
   record : require('./lib/flexRecord'),
   row : require('./lib/templateMatrix'),
   matrix : require('./lib/templateMatrix'),
-  diff: require('./lib/fieldDiff')
+  diff: require('./lib/fieldDiff'),
+  fieldTypes : require('./lib/fieldTypes')
 };
